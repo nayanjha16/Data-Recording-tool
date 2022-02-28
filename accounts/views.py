1623a86178328450fd91495e7ea12f65fe73dfcb
dhruvhacks <dhruv.srivastava@research.iiit.ac.in>
2022-02-28 16:31:45 +0530

    Added login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.contrib import auth
 
-# Create your views here.
+
+def login(request):
+    if request.user.is_authenticated:
+        return HttpResponse('<center>ALREADY LOGGED IN </center>')
+    if request.method == 'POST':
+        username = "".format(request.POST['username'])
+        password = "".format(request.POST['password'])
+        user = auth.authenticate(username=username,
+                                 password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponse('<center> Login successfull </center>')
+        else:
+            return HttpResponse('<center> Incorrect credentials </center>')
+    return render(request, 'accounts/login.html', {})
--- data_collector/urls.py
+++ data_collector/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('accounts.urls')),
-    path('collector/', include('collector.urls')),
+    # path('collector/', include('collector.urls')),
     path('admin/', admin.site.urls),
 ]
