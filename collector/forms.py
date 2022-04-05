2881b783f3831caa81f0212735e0083da7d728aa
dhruvhacks <dhruv.srivastava@research.iiit.ac.in>
2022-04-06 01:56:40 +0530

    Removed comment field from audio_files model

--- collector/forms.py
+++ collector/forms.py
@@ -6,7 +6,6 @@ class audio_upload_form(forms.ModelForm):
     class Meta:
         model = models.audio_files
         fields = [
-            'comments',
             'audio'
         ]
         exclude = [
--- collector/models.py
+++ collector/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 
 class sentences(models.Model):
-    TOTAL_PROMPTS = 0
     sentence = models.CharField(max_length=5000)
 
     def __str__(self):
@@ -15,7 +14,6 @@ class audio_files(models.Model):
                              on_delete=models.CASCADE)
     sentence = models.ForeignKey(sentences,
                                  on_delete=models.CASCADE)
-    comments = models.CharField(max_length=500, blank=True)
     audio = models.FileField(upload_to="audio",
                              blank=False,
                              null=False)
